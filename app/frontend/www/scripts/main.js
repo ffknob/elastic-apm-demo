e840f6c1451f6139d1ce3310c70a11f6c122dd10
ffknob <ffknob@tce.rs.gov.br>
2019-07-25 17:54:57 -0300

    table

--- app/frontend/www/scripts/main.js
+++ app/frontend/www/scripts/main.js
@@ -1,9 +1,13 @@
+let simulationId = 1;
 let simulations$ = new rxjs.Subject();
 
 function createSimulation(event) {
     const numberOfRequests = document.querySelector('#select-number-of-requests').value;
     const maxRandomDelay = document.querySelector('#input-max-random-delay').value;
+    
+    simulationId += 1;
     const simulation = {
+        id: simulationId,
         type: 'success',
         options: {
             numberOfRequests: numberOfRequests,
@@ -12,7 +16,9 @@ function createSimulation(event) {
         requests: {
             total: numberOfRequests,
             sent: 0,
+            sent$: new rxjs.Subject(),
             completed: 0,
+            completed$: new rxjs.Subject(),
             requests$: new rxjs.Subject()
         },
         srcElement: {
@@ -24,11 +30,50 @@ function createSimulation(event) {
     return simulation;
 }
 
+function createRequestsTableRow(simulation) {
+    const requestsTableBodyOld = document.querySelector('#table-requests tbody');
+    const requestsTableBodyNew = document.createElement('tbody');
+    const requestsTableRows = document.querySelectorAll('#table-requests tbody tr');
+    const requestRow = document.createElement('tr');
+    const requestCellId = document.createElement('th');
+    const requestCellType = document.createElement('td');
+    const requestCellRandomDelay = document.createElement('td');
+    const requestCellNumberOfRequests = document.createElement('td');
+    const requestCellSent = document.createElement('td');
+    const requestCellCompleted = document.createElement('td');
+
+    requestCellId.innerHTML = simulation.id;
+    requestCellType.innerHTML = simulation.type;
+    requestCellRandomDelay.innerHTML = simulation.options.maxRandomDelay;
+    requestCellNumberOfRequests.innerHTML = simulation.requests.total;
+    requestCellSent.innerHTML = simulation.requests.sent;
+    simulation.requests.sent$.subscribe((sent) => requestCellSent.innerHTML = sent);
+    requestCellCompleted.innerHTML = simulation.requests.completed;
+    simulation.requests.completed$.subscribe((completed) => requestCellCompleted.innerHTML = completed);
+
+    requestRow.appendChild(requestCellId);
+    requestRow.appendChild(requestCellType);
+    requestRow.appendChild(requestCellNumberOfRequests);
+    requestRow.appendChild(requestCellRandomDelay);
+    requestRow.appendChild(requestCellSent);
+    requestRow.appendChild(requestCellCompleted);
+
+    requestsTableBodyNew.appendChild(requestRow)
+    if (requestsTableRows.length > 0) {
+        console.log(requestsTableRows);
+        requestsTableRows.forEach((node) => requestsTableBodyNew.appendChild(node));
+    }
+
+    requestsTableBodyOld.parentNode.replaceChild(requestsTableBodyNew, requestsTableBodyOld)
+}
+
 function makeRequest(simulation) {
     const randomDelay = Math.floor(Math.random() * (simulation.options.maxRandomDelay));
     const requestObservable = rxjs.timer(randomDelay);
 
+    simulation.requests.randomDelay = randomDelay;
     simulation.requests.sent += 1;
+    simulation.requests.sent$.next(simulation.requests.sent);
 
     requestObservable.subscribe(() => {
         treatResponse(simulation);
@@ -41,14 +86,14 @@ function treatResponse(simulation) {
     const srcElement = document.querySelector('#' + simulation.srcElement.id);
 
     simulation.requests.completed += 1;
+    simulation.requests.completed$.next(simulation.requests.completed);
+
     srcElement.innerText = `${simulation.requests.completed}/${simulation.requests.total}`;
 
     if (simulation.requests.completed == simulation.requests.total) {
         srcElement.disabled = false;
         srcElement.innerText = simulation.srcElement.innerText;
     }
-
-    //console.log(`${simulation.requests.completed}/${simulation.requests.total}`);
 }
 
 function startSimulation(simulation) {
@@ -66,6 +111,8 @@ function endSimulation(simulation) {
 
 function simulate(event) {
     const simulation = createSimulation(event);
+
+    createRequestsTableRow(simulation);
 
     simulations$.next(simulation);
 }
